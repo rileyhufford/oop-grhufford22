4c3eef507c0a71c60fb89d979fbde0c0c665fd9a
BuildTools <unconfigured@null.spigotmc.org>
2022-02-11 11:33:43 -0700

    fixing testing for docker

---
+++ js/baseballhat.mjs
@@ -0,0 +1,31 @@
+export class baseballhat
+{
+    constructor(mainColor, type, strapType)
+    {
+        this._mainColor = null;
+        this.mainColor = mainColor;
+        this._type = null;
+        this.type = type;
+        this._strapType = null;
+        this.strapType = strapType;
+    }
+    get mainColor() {return this._mainColor;}
+    set mainColor(value)
+    {
+
+    }
+
+    get type() {return this._type;}
+    set type(value)
+    {
+
+    }
+
+    get strapType() {return this._strapType;}
+    set strapType(value)
+    {
+
+    }
+
+}
+
