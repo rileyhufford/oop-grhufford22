c26ee8e683d00dc37e65c81dd6dd009b80b80fa3
BuildTools <unconfigured@null.spigotmc.org>
2022-02-11 11:46:12 -0700

    made more readable

--- js/test/test.mjs
+++ js/test/test.mjs
@@ -2,24 +2,25 @@
 
 
 import {Cord, Equipment} from '../cord.mjs'
-
-// import { assert } from 'assert'
 import { assert, expect, should } from 'chai';
+
 
 describe('Cord', ()=>{
   describe('length', ()=>{
     it('is the first constructor parameter ',()=>{
       const cord = new Cord(1,"male plug",3);
-      assert(cord.length,1);
+      expect(cord.length).to.equal(1); //nice to use expect (over assert) because it is easier
+      //                                 to read for more nontechnial people
     })
     it('can be changed',()=>{
       const cord = new Cord(1,"male plug",3);
       cord.length = 2.5;
-      assert(cord.length,2.5);
+      expect(cord.length).to.equal(2.5);
     })
     it('must be positive',()=>{
       const cord = new Cord(1,"male plug",3);
-      expect(()=>{cord.length = 0.0;})
+      const expectArg = ()=>{cord.length = 0.0}; //lambda expression to call itself or something
+      expect(expectArg)
         .to.throw("length must be positive")
     });
   }); //describe length
