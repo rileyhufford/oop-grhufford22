3da4f77ce2c571fc47f7419e5044322b846d6753
BuildTools <unconfigured@null.spigotmc.org>
2022-02-18 11:56:55 -0700

    added testing for cpp cord

--- cpp/src/cord.cpp
+++ cpp/src/cord.cpp
@@ -1,33 +1,31 @@
 #include "cord.h"
+
 #include <stdexcept>
 
 namespace power
 {
-    static double checkedLength(double length)
-    {
+    static double checkedLength(double length) {
         if (length > 0.0)
         {
             return length;
         }
         else
         {
-            throw new std::out_of_range("length must be positive");
+            throw std::out_of_range("length must be positive");
         }
     }
-
     Cord::Cord(double length, const std::string &connector, double capacity)
-        : m_length(checkedLength(length)), m_connector(connector), m_capacity(capacity) // this is the intializer and you can intialize
-                                                                                        // the values you want in them
+        : m_length(checkedLength(length)), m_connector(connector), m_capacity(capacity)
     {
-        //constructor
     }
+
     double Cord::length() const
     {
         return m_length;
     }
+
     void Cord::length(double value)
     {
-        m_length = checkedLength(value);
+        m_length=checkedLength(value);
     }
-
 }
