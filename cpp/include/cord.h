3325cdad4800c747ce9f8759b11e22c409f30399
BuildTools <unconfigured@null.spigotmc.org>
2022-02-25 11:51:42 -0700

    updated 2-25-22

--- cpp/include/cord.h
+++ cpp/include/cord.h
@@ -9,15 +9,19 @@ namespace power
 {
     class Cord
     {
+        public: const int m_constructedOn;
         //default private, while type struct is default public
         private: double m_length;
         private: std::string m_connector;
         private: double m_capacity;
 
-        public: Cord(double length, const std::string &connector, double capacity); //constructor
+        public: Cord(int constructedOn, double length, const std::string &connector, double capacity); //constructor
 
         public: double length() const; //pretty much a getter, but literally a method that returns a double
         public: void length( double value); //pretty much a setter, but literally a method
+
+        //destructor '~' is the cpp identifier
+        public: ~Cord();
 
     };
     typedef std::shared_ptr < Cord > SPCord;
--- cpp/src/test_memory.cpp
+++ cpp/src/test_memory.cpp
@@ -12,27 +12,61 @@
 // CONSTANTS
 // CODE/TEXT
 //
+#include "gtest/gtest.h"
 
 #include "cord.h"
+#include <memory>
+#include <iostream>
+
 
 class FusedCord : public power::Cord
 {
-    private: m_fuseOk; //automatic
+    private: bool m_fuseOk; //automatic
+    private: std::string m_fuseType;
+
+    public: FusedCord(int line, double length, const std::string &connector, double capacity)
+        : Cord(line, length, connector, capacity)
+    {
+        m_fuseOk = true;
+        m_fuseType = "funnest type!";
+        std::cout << "FusedCord@" << (void*) this <<"from" << m_constructedOn << "constructed." << std::endl;
+    }
+
+    public: ~FusedCord()
+    {
+        std::cout << "FusedCord@" << (void*) this <<"from" << m_constructedOn << "destructed." << std::endl;
+    }
+
+
+    
     // ...
 
 };
 
+// root = a->b->c->d  //ref count is used to clean up b,c,d when deleting a
+// a<->b<->c<->d
 using namespace power;
-TEST(FusedCord, Ok)
+using namespace std;
+TEST(FusedCord,Ok)
 {
-    FusedCord fusedCord; //automatic
-    FusedCord *pFusedCord = new FusedCord();
-    Cord cord = FusedCord(); //compiles but almost certaintly broken (object truncation)
+    double length = 15.0;
+    string connector = "female 3-prong";
+    double capacity = 20.0;
+    
+    
+    FusedCord fusedCord(__LINE__, length, connector, capacity); //automatic
+    FusedCord *pFusedCord = new FusedCord(__LINE__, length, connector, capacity);
+    Cord cord = FusedCord(__LINE__, length, connector, capacity); //compiles but almost certaintly broken (object truncation)
 
-    Cord *pCord = new FusedCord(); //better
+    Cord *pCord = new FusedCord(__LINE__, length, connector, capacity); //better
     // .. delete pCord;
-    SPCord spCord (new FusedCord()); //correct
+    ASSERT_EQ(cord.length(), length()+1);
+    SPCord spCord (new FusedCord(__LINE__, length, connector, capacity)); //correct
+    ASSERT_EQ(cord.length(), length +1);
 
-    spCord ->length();
+    spCord = SPCord(new FusedCord(__LINE__, length, connector, capacity));
+
+    delete (FusedCord*)pCord;
+    close();
     // ...
 }
