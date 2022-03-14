792040cbde7fe819d5c5186c19be0c2ab1298e8a
BuildTools <unconfigured@null.spigotmc.org>
2022-03-14 11:54:12 -0600

    ploymorphism

--- cpp/src/main_theory.cpp
+++ cpp/src/main_theory.cpp
@@ -1,128 +1,200 @@
-// what's going on in C++ for 00 stuff
+// what's going on in C++ for OO stuff?
 #include <stdlib.h>
 #include <iostream>
+#include <memory>
+#include <stdexcept>
+#include <functional>
 
 using namespace std;
 
 struct Equipment;
+struct Cord;
 
-struct Cord 
-{
-    int x;
-    bool compatiable(Equipment &equipment) { return true; }
-    Cord(int _x) : x(_x) {}
+struct Cord {
+    const int capacity;
+    Cord(int _capacity) : capacity(_capacity) {} 
+    virtual bool compatible(const Equipment &equipment) const;
+    virtual int watts() const { return 120*capacity; }
+    virtual ~Cord() { cout << "~Cord()" << endl; } // if you have virtual methods, you should have this virtual destructor
 };
 
-struct Equipment
-{
-    bool plugged;
-    Equipment() { plugged = false;}
-    void plug(Cord &cord)
-    {
-        if(cord.compatiable(*this))
-        {
-            plugged = true;
-        }
-    }
+struct Equipment {
+    const int consumption;
+    Equipment(int _consumption) : consumption(_consumption) { }
 };
 
-bool Cord::compatiable(Equipment &equipment)
-{
-    if(!equipment.plugged && x != 3)
-    {
-        // ... 
-        return true;
-    }
-    return false;
+bool Cord::compatible(const Equipment &equipment) const {
+  cout << "Cord::compatible()" << endl;
+  return (capacity >= equipment.consumption);
 }
 
 struct CCord;
-struct CCord_vftbl
-{
-    bool (*compatiable)(CCord *me, Equipment &equipment); //pointer to the cord ('this' pointer)
-    void (*destructon)(CCord *me); //clean up
+struct CCord_vftbl {
+   void (*destructor)(CCord *me);
+   bool (*compatible)(const CCord *me, const Equipment &equipment);
+   int (*watts)(const CCord *me);
+   //Only one virtual table is needed per class, not per instance
+   //How the class implements these things, declaration of whats needed
 };
 
-struct CCord
-{
+struct CCord {
     CCord_vftbl *vftbl;
-    int x;
+    int capacity;
+    //Every cord needs a pointer to the vf table, and a capacity
 };
 
-struct CFusedCord
-{
-    // CFusedCord_vftbl *vftbl;
-    int x;
-    bool fuseOk;
-};
 
-bool CCord_compataible(CCord *me, Equipment &equipment)
-{
-    if(!equipment.plugged && me->x != 3)
-    {
-        return true;
-    }
-    return false;
+void CCord_destructor(CCord *me) {
+  cout << "CCord_destructor()" << endl;    
+  free(me);
 }
 
-struct FusedCord : Cord
-{
-    bool fuseOk;
-    FusedCord(int x) : Cord(x) { fuseOk = true; }
+bool CCord_compatible(const CCord *me, const Equipment &equipment) {
+  cout << "CCord_compatible()" << endl;  
+  return (me->capacity >= equipment.consumption);
+}
 
-    // override
-    bool compatiable(Equipment &equipment)
-    {
-        cout << "FusedCord::compatiable()" << endl;
+int CCord_watts(const CCord *me) {
+  return me->capacity*120;
+}
+
+CCord_vftbl cCord_vftbl = {
+    &CCord_destructor,
+    &CCord_compatible,
+    &CCord_watts
+    //This is the one virtual table that is needed for the cord class
+};
+
+CCord *CCord_constructor(CCord *me, int _capacity) {
+  if (me == NULL) {
+    me = (CCord*) malloc(sizeof(CCord)); //accounting for NULL
+  }
+
+  me->vftbl=&cCord_vftbl; //adjusting the pointer to point to the vf table
+  me->capacity=_capacity;
+  return me;
+}
+
+struct FusedCord : Cord {
+    bool fuseOk; //extra type that fused cord has
+    FusedCord(int capacity) : Cord(capacity), fuseOk(true) {  }
+
+    virtual bool compatible(const Equipment &equipment) const override {
+        cout << "FusedCord::compatible()" << endl;
         if (!fuseOk) return false;
-        else Cord::compatiable(equipment);
+        return Cord::compatible(equipment);
     }
+
+    virtual void reset() { fuseOk = true; } //unique fusedcord fcn
+    virtual void trip() { fuseOk = false; } //unique fusedcord fcn
+    virtual ~FusedCord() { cout << "~FusedCord()" << endl; } //Implied that the Cord destructor will be called after,
+                                                            // because a fusedcord is also a cord
 };
 
-bool CFusedCord_compatiable(CFusedCord *me, Equipment &equipment)
-{
-    if(!me->fuseOk) { return false; }
-    CCord_compataible(me, equipment);
-}
+struct CFusedCord;
+struct CFusedCord_vftbl {
+  void (*destructor)(CFusedCord *me);
+  bool (*compatible)(const CFusedCord *me, const Equipment &equipment);
+  int (*watts)(const CFusedCord *me); //exactly the same (order and types as well), but pointers changed
 
-void CCord_destructor(CCord *me)
-{
-    free(me);
-}
-
-CCord_vftbl cCord_vftbl = 
-{
-    &CCord_compataible,
-    &CCord_destructor
+  //unique to fusedcord
+  void (*reset)(CFusedCord *me); 
+  void (*trip)(CFusedCord *me);  
 };
 
-CCord *CCordConstructor(int _x)
-{
-    CCord *me = (CCord*) malloc(sizeof(CCord));
+struct CFusedCord {
+  CFusedCord_vftbl *vftbl; //pretty much same pointer, but pointing to a bigger table
+  int capacity; //data matches exactly with a regular cord's vf table from here
+  //A cord class can point to a fusedcord vf table, because the same data lives inside the fusedcord vf table
 
-    me->vftbl=&cCord_vftbl;
-    me->x=_x;
-    return me;
+  bool fuseOk;
+};
+
+void CFusedCord_destructor(CFusedCord *me) {
+  cout << "CFusedCord_destructor" << endl;
+  CCord_destructor((CCord*)me);
 }
 
-void foo()
-{
-    Cord *cppCord = new Cord(3);
-    FusedCord *cppFusedCord = new FusedCord(33);
-    CCord *cCord = CCordConstructor(3);
-
-    Equipment *equipment = new Equipment();
-    //cout << cppCord->compatiable(*equipment) << endl;
-    //cout << dynamic_cast<Cord*>(cppFusedCord)->compatiable(*equipment) << endl;
-    cout << dynamic_cast<Cord*>(cppFusedCord)->compatiable(*equipment) << endl;
-
-    cout << cCord->vftbl->compatiable(cCord, *equipment) << endl;
-
-    cout << CCord_compatiable(cCord, *equipment) << endl;
+bool CFusedCord_compatible(const CFusedCord *me, const Equipment &equipment) {
+  cout << "CFusedCord_compatible()" << endl;
+  if (!me->fuseOk) { return false; }
+  return CCord_compatible((const CCord*)me,equipment); //reusing cord's compatiable and casting fusedcord pointers,
+                                                        //to cord tpye pointers
 }
 
-int main()
+void CFusedCord_reset(CFusedCord *me) {
+  me->fuseOk = true;
+}
+
+void CFusedCord_trip(CFusedCord *me) {
+  me->fuseOk = false;
+}
+
+CFusedCord_vftbl cFusedCord_vftbl =
+  {
+   &CFusedCord_destructor,
+   &CFusedCord_compatible,
+   (int (*)(const CFusedCord*))&CCord_watts, // not overriden, calling the same fcn of watts
+                                            // casting to a fusedcord pointer
+   &CFusedCord_reset,
+   &CFusedCord_trip
+  };
+
+
+CFusedCord *CFusedCord_constructor(CFusedCord *me, int capacity) {
+  if (me == NULL) {
+    me = (CFusedCord*) malloc(sizeof(CFusedCord));
+  }
+  CCord_constructor((CCord*)me,capacity); //call as base class constructor on myself, cast me to cord
+  me->vftbl=&cFusedCord_vftbl; //overrides the cord vf table and makes it a fusedcord vf table
+  me->fuseOk=true;
+  return me;
+}
+
+struct lastly
 {
-    foo();
-    return 0;
+  operator bool() { throw std::runtime_error(""); }
+  std::function<void()> fini;
+  explicit lastly(const std::function<void()> &_fini) : fini(_fini) {}
+  lastly(const lastly &copy) = delete;
+  lastly &operator=(const lastly &assign) = delete;  
+  ~lastly() { fini(); }
+};
+
+void foo() {
+    Cord *cppCord = new Cord(10);
+    lastly deleteCppCord([&] { delete cppCord; });
+    FusedCord *cppFusedCord = new FusedCord(15);
+    lastly deleteCppFusedCord([&] { delete cppFusedCord; });    
+    CCord *cCord = CCord_constructor(NULL,10);
+    lastly destructCCord([&] { cCord->vftbl->destructor(cCord); });
+    CFusedCord *cFusedCord = CFusedCord_constructor(NULL,15);
+    lastly destructCFusedCord([&] { cFusedCord->vftbl->destructor(cFusedCord); });
+
+    cout << cppCord->watts() << endl;
+    cout << cppFusedCord->watts() << endl;    
+    cout << cCord->vftbl->watts(cCord) << endl; //handing myself in as the pointer needed for watts
+    cout << cFusedCord->vftbl->watts(cFusedCord) << endl;    
+    
+    Equipment *equipment = new Equipment(15);
+
+    cout << cppCord->compatible(*equipment) << endl;
+    cout << cppFusedCord->compatible(*equipment) << endl;
+    cout << cCord->vftbl->compatible(cCord,*equipment) << endl;
+    cout << cFusedCord->vftbl->compatible(cFusedCord,*equipment) << endl; 
+
+    cppFusedCord->trip();
+    cFusedCord->vftbl->trip(cFusedCord);
+
+    Cord *cppFusedCordAsCord = dynamic_cast<Cord*>(cppFusedCord);
+    CCord *cFusedCordAsCCord = (CCord*)(cFusedCord);
+    cout << cppFusedCordAsCord->compatible(*equipment) << endl;
+    cout << cFusedCordAsCCord->vftbl->compatible(cFusedCordAsCCord,*equipment) << endl; //will use the correct compatiable
+    //polymorphism shown here with compatible   
+    
+}
+
+int main() {
+  foo();
+  return 0;
 }
