540616a857950d8e3d719129f45551b9b4b064ec
BuildTools <unconfigured@null.spigotmc.org>
2022-05-16 12:14:53 -0600

    diamondhw added

--- cpphw/src/main_diamondhw.cpp
+++ cpphw/src/main_diamondhw.cpp
@@ -20,66 +20,70 @@ struct Camera
     private: char m_multicamLabel; //a cam, b cam, c cam, etc.
     //used when there are multiple cameras in a production. EX: a cam speeding, b cam cut, etc.
     public: 
-        Camera()
-        {
-            cout << "Camera()" << endl;
-        }
-    virtual char Cbrand() const {return m_multicamLabel;}
-    virtual void Cbrand(const char &value) {m_multicamLabel = value;}
+        Camera() {cout << "Camera() constructor" << endl;}
+    virtual char multicamLabel() const {return m_multicamLabel;}
+    virtual void multicamLabel(const char &value) {m_multicamLabel = value;}
     virtual ~Camera() { cout << "~Camera()" << endl;}
 };
 
 struct Body;
 struct Lens;
 
+struct ConnectedLens : virtual Camera
+{
+    bool lensPowered;
+    ConnectedLens(bool _lensPowered) : lensPowered(_lensPowered) {cout << "ConnectedLens() constructor" << endl;}
+};
+
 struct Body: virtual Camera
 {
     const string brand;
-    //const int focalLength;
-    //Body(string _mount, int _focalLength) : mount(_mount), focalLength(_focalLength) { cout << "Body" << endl;}
-    Body(string _brand) : brand(_brand) { cout << "Body" << endl;}
+    Body(string _brand) : brand(_brand) { cout << "Body() constructor" << endl;}
 
     virtual bool lensAttached(const ConnectedLens &connectedlens) const;
     virtual ~Body() { cout << "~Body()" << endl;}
 };
 
-struct ConnectedLens : virtual Camera
-{
-    const bool lensPowered;
-    ConnectedLens(bool _lensPowered) : lensPowered(_lensPowered) {}
-};
 
-bool Body::lensAttached(const ConnectedLens &connectedlens) const
-{
+
+bool Body::lensAttached(const ConnectedLens &connectedlens) const{
     cout << "Body::lensAttached()" << endl;
-    return connectedlens.lensPowered;
+    return (connectedlens.lensPowered);
 }
 
 
 struct Lens : virtual Camera
 {
     bool mounted;
-    Lens() : mounted(true) { cout << "ConnectedLens" << endl;}
+    Lens() : mounted(true) { cout << "Lens() constructor" << endl;}
     virtual void error() {mounted = false;}
     virtual void reset() {mounted = true;}
 
-    virtual ~Lens(){cout << "~ConnectedLens()" << endl;}
+    virtual ~Lens(){cout << "~Lens()" << endl;}
 };
 
 
 struct DSLR : Body, Lens, virtual Camera
 {
-    DSLR(string brand) : Body(brand) { cout << "DSLR" << endl;}
-    virtual bool lensAttached(const ConnectedLens &connectedlens) const override
-    {
+    DSLR(string brand) : Body(brand) { cout << "DSLR() constructor" << endl;}
+    virtual bool lensAttached(const ConnectedLens &connectedlens) const{
         cout << "DSLR::lensAttached()" << endl;
         if(!mounted) return false;
         return Body::lensAttached(connectedlens);
     }
-    ~DSLR() {cout << "~DSLR" << endl;}
+    ~DSLR() {cout << "~DSLR()" << endl;}
 };
 
-int main(int argc, const char *argv[])
+void foo()
 {
-    DSLR myDSLR("ef");
+    bool lenspowered = true;
+    DSLR dslr("Canon");
+    ConnectedLens connectedlens(lenspowered);
+    dslr.lensAttached(connectedlens);
+}
+
+int main()
+{
+    foo();
+    return 0;
 }
